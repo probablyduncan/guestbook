1930b9912049ba255f017fed480953fadf3075ee
probablyduncan <duncanpetrie1@gmail.com>
2025-07-11 13:46:06 +0100

    add mycursor

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import cloudflareLogo from './assets/Cloudflare_Logo.svg'
-import './App.css'
 import { createSignal } from 'solid-js'
+import { MyCursor } from './components/MyCursor';
 
 function App() {
   const [count, setCount] = createSignal(0);
@@ -10,47 +7,23 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href='https://vite.dev' target='_blank'>
-          <img src={viteLogo} class='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} class='logo react' alt='React logo' />
-        </a>
-        <a href='https://workers.cloudflare.com/' target='_blank'>
-          <img src={cloudflareLogo} class='logo cloudflare' alt='Cloudflare logo' />
-        </a>
-      </div>
-      <h1>Vite + React + Cloudflare</h1>
-      <div class='card'>
-        <button
-          onClick={() => setCount((count) => count + 1)}
-          aria-label='increment'
-        >
-          count is {count()}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <div class='card'>
-        <button
-          onClick={() => {
-            fetch('/api/')
-              .then((res) => res.json() as Promise<{ name: string }>)
-              .then((data) => setName(data.name))
-          }}
-          aria-label='get name'
-        >
-          Hotspot name is: {name()}
-        </button>
-        <p>
-          Edit <code>worker/index.ts</code> to change the name
-        </p>
-      </div>
-      <p class='read-the-docs'>
-        Click on the Vite and React logos to learn more
-      </p>
+      <MyCursor />
+      <button
+        onClick={() => setCount((count) => count + 1)}
+        aria-label='increment'
+      >
+        count is {count()}
+      </button>
+      <button
+        onClick={() => {
+          fetch('/api/')
+            .then((res) => res.json() as Promise<{ name: string }>)
+            .then((data) => setName(data.name))
+        }}
+        aria-label='get name'
+      >
+        Hotspot name is: {name()}
+      </button>
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import './index.css'
 import App from './App.tsx'
 import { render } from 'solid-js/web'
 
